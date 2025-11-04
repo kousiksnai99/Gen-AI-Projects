9607c5ddc51034c3d34a0850701764e557f496bf
Kousik Snai <56123189+kousiksnai99@users.noreply.github.com>
2025-11-04 15:20:10 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -110,3 +110,60 @@ def create_new_runbook(runbook_name, system_name):
         print(f"Runbook published successfully: {file_name}")
     except Exception as e:
         print(f"Error publishing runbook: {e}")
+
+
+
+
+
+
+
+
+################################################################################################# 
+## Project name : Agentic AI POC                                                                #
+## Business owner , Team : Data and AIA                                                         #
+## Notebook Author , Team: POC Team                                                             #
+## Date: 29th Oct 2025                                                                          #
+## Puprose of Notebook: This file contains the function that will edit the runbook in AA.       #
+#################################################################################################
+
+# # Load all the libraries
+import config
+from azure.identity import DefaultAzureCredential, AzureCliCredential
+from azure.mgmt.automation import AutomationClient
+from datetime import datetime
+import requests
+def create_new_runbook(runbook_name, system_name):
+    cred=DefaultAzureCredential()
+    client=AutomationClient(cred, config.SUBSCRIPTION_ID)
+    time_stamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    runbook=client.runbook.create_or_update(
+        resource_group_name=config.RESOURCE_GROUP,
+        automation_account_name= config.AUTOMATION_ACCOUNT,
+        runbook_name=f"{runbook_name}_{system_name}_{time_stamp}",
+        parameters={
+            "location":config.LOCATION,
+            "name": f"{runbook_name}_{system_name}_{time_stamp}",
+            "properties":{
+                "runbookType":"PowerShell",
+                "logProgress": True,
+                "logVerbose": False,
+                },
+                },
+                )
+
+def get_source_content(runbook_name):
+    credential=DefaultAzureCredential()
+    client=AutomationClient(credential, config.SUBSCRIPTION_ID)
+    token=credential.get_token("https://management.azure.com/.default").token
+    url=f'https://management.azure.com/subscriptions/{config.SUBSCRIPTION_ID}/resourceGroups/{config.RESOURCE_GROUP}/providers/Microsoft.Automation/automationAccounts/{config.AUTOMATION_ACCOUNT}/runbooks/{runbook_name}/content?api-version=2024-10-23'
+    headers={
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/octet-stream"
+    }
+    response=requests.get(url, headers=headers)
+    print(response)
+    if response.status_code==200:
+        runbook_content = response.content.decode("utf-8", errors='ignore')
+        print(runbook_content)
+    else:
+        print("Failed")
